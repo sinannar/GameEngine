d76d27ca48cbbbc92df2d7b5d2cb16314aee1fa3
sinan nar <sinan.nar@gmail.com>
2018-04-02 22:00:53 +1200

    matrix implementation

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,7 +1,9 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -40,13 +42,15 @@ public class MainGameLoop {
 		
 		RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
-		TexturedModel texturedModel = new TexturedModel(model,texture);
+		TexturedModel staticModel = new TexturedModel(model,texture);
+		
+		Entity entity = new Entity(staticModel, new Vector3f(-1,0,0), 0,0,0,1);
 		
 		while(!Display.isCloseRequested()){
 			//game logic
 			renderer.prepare();
 			shader.start();
-			renderer.render(texturedModel);
+			renderer.render(entity,shader);
 			shader.stop();
 			DisplayManager.updateDisplay();			
 		}
