aaeeb8713bcdd2637a477e8650c60f993646071c
Sinan NAR <sinan.nar@outlook.com>
2018-04-02 20:52:27 +1200

    uniform matrix passing to shaders started

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,10 +1,14 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram{
 	
 	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
 	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
 
+	private int location_transformationmatrix;
+	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
 	}
@@ -14,7 +18,15 @@ public class StaticShader extends ShaderProgram{
 		super.bindAttribute(0, "position");
 		super.bindAttribute(1, "textureCoords");
 	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationmatrix = super.getUniformLocation("transformationMatrix");		
+	}
 	
-	
+	public void loadTransformationmatrix(Matrix4f matrix)
+	{
+		super.loadMatrix(location_transformationmatrix, matrix);
+	}
 
 }
