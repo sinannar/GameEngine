45e77f7c754996157c777dab457aed085c450eed
sinan nar <sinan.nar@gmail.com>
2018-04-19 12:49:44 +1200

    fog implementation has been done

--- src/renderEngine/MasterRenderer.java
+++ src/renderEngine/MasterRenderer.java
@@ -23,6 +23,10 @@ public class MasterRenderer {
 	private static final float NEAR_PLANE = 0.1f;
 	private static final float FAR_PLANE = 1000f;
 	
+	private static final float RED = 0.5f;
+	private static final float GREEN = 0.5f;
+	private static final float BLUE = 0.5f;
+	
 	private Matrix4f projectionMatrix;
 
 	private StaticShader shader = new StaticShader();
@@ -57,12 +61,14 @@ public class MasterRenderer {
 	{
 		prepare();
 		shader.start();
+		shader.loadSkyColor(RED, GREEN, BLUE);
 		shader.loadLight(sun);
 		shader.loadViewMatrix(camera);
 		renderer.render(entities);
 		shader.stop();
 		
 		terrainShader.start();
+		shader.loadSkyColor(RED, GREEN, BLUE);
 		terrainShader.loadLight(sun);
 		terrainShader.loadViewMatrix(camera);
 		terrainRenderer.render(terrains);
@@ -102,7 +108,7 @@ public class MasterRenderer {
 	public void prepare(){
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
-		GL11.glClearColor(0.4f, 0.0f, 0.0f, 1);
+		GL11.glClearColor(RED, GREEN, BLUE, 1);
 	}
 	
 	private void createProjectionMatrix(){
--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,6 +1,7 @@
 package shaders;
 
 import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
 
 import entities.Camera;
 import entities.Light;
@@ -19,6 +20,7 @@ public class StaticShader extends ShaderProgram{
 	private int location_shineDamper;
 	private int location_reflectivity;
 	private int location_useFakeLighting;
+	private int location_skyColour;
 	
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -52,6 +54,12 @@ public class StaticShader extends ShaderProgram{
 		location_shineDamper = super.getUniformLocation("shineDamper");
 		location_reflectivity = super.getUniformLocation("reflectivity");
 		location_useFakeLighting = super.getUniformLocation("useFakeLighting");
+		location_skyColour = super.getUniformLocation("skyColour");
+	}
+	
+	public void loadSkyColor(float r, float g, float b)
+	{
+		super.loadVector(location_skyColour, new Vector3f(r,g,b));
 	}
 	
 	public void loadLight(Light light)
