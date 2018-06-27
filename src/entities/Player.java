5891e8ecbb1e643d6debac5128a62337e08e4184
sinan nar <sinan.nar@gmail.com>
2018-06-27 17:49:18 +1200

    jump implemented - but you can jump infinite time there is no limit

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -10,8 +10,14 @@ public class Player extends Entity{
 
 	private static final float RUN_SPEED = 20;
 	private static final float TURN_SPEED = 160;
+	private static final float GRAVITY = -50;
+	private static final float JUMP_POWER = 30;
+	
+	private static final float TERRAIN_HEIGHT = 0;
+	
 	private float currentSpeed = 0;
 	private float currentTurnSpeed = 0;
+	private float upwardsSpeed = 0;
 	
 	public Player(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
 		super(model, position, rotX, rotY, rotZ, scale);
@@ -25,6 +31,17 @@ public class Player extends Entity{
 		float dx = (float) (distance * Math.sin(Math.toRadians(super.getRotY())));
 		float dz = (float) (distance * Math.cos(Math.toRadians(super.getRotY())));
 		super.increatePosition(dx, 0, dz);
+		
+		upwardsSpeed += GRAVITY * DisplayManager.getFrameTimeSeconds();
+		super.increatePosition(0, upwardsSpeed * DisplayManager.getFrameTimeSeconds(), 0);
+		if(super.getPosition().y<TERRAIN_HEIGHT) {
+			upwardsSpeed = 0;
+			super.getPosition().y = TERRAIN_HEIGHT;
+		}
+	}
+	
+	private void jump() {
+		this.upwardsSpeed = JUMP_POWER;
 	}
 	
 	private void checkInputs() {
@@ -48,6 +65,10 @@ public class Player extends Entity{
 			this.currentTurnSpeed = TURN_SPEED;
 		} else {
 			this.currentTurnSpeed = 0;
+		}
+		
+		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			jump();
 		}
 		
 	}
